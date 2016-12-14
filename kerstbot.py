ad953681bb0da2cfb60f7839381de2a29e6c430e
root <mark@greenorange.com>
2016-12-14 10:07:12 +0000

    liefdeee

--- kerstbot.py
+++ kerstbot.py
@@ -758,7 +758,7 @@ def send_message(token, recipient, text, data):
       elif data['oldmessage'] == 'Leuk om je te helpen bij het samenstellen van het kerstmenu. Mag ik vragen of het een vegetarisch, of een vlees/vis menu moet worden?':
 
           if text == 'Vlees/Vis':
-              message = 'Wil je dan Vlees, Vis of een combinatie?'
+              message = 'Wil je dan Vlees, Vis of heb je geen voorkeur?'
               data = messageSend(recipient,message, token,data)
               quicks = ['Vis', 'Vis en Vlees', 'Vlees']
               sendImage(recipient, 'https://s28.postimg.org/xft4djma5/IG_vis_vlees.png')
@@ -773,7 +773,7 @@ def send_message(token, recipient, text, data):
               sendImage(recipient, 'https://s23.postimg.org/6m9a2e7uz/IG_cake_ijs.png')
               sendQuicks(recipient, message, quicks)
               mg.updateUser(recipient, data)
-      elif data['oldmessage'] ==  'Wil je dan Vlees, Vis of een combinatie?':
+      elif data['oldmessage'] ==  'Wil je dan Vlees, Vis of heb je geen voorkeur?':
           if text == 'Vis en Vlees':
               data['data']['voorkeur'] = ['Vis', 'Vlees']
           else:
